9baaa9b360b4a90101fc83000ae20a51c7d64802
Jack Mallahan <jmallahan@captechconsulting.com>
2019-02-11 13:26:14 -0700

    Please work

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/ruraltracklottery'));
 
 app.get('/*', function (req, res) {
-
   res.sendFile(path.join(__dirname + '/dist/ruraltracklottery/index.html'));
 });
 
